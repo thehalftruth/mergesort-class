a679d4df87e05f6b2e5943030897bf4737a112c3
thehalftruth <adrianhaider@gmx.de>
2015-04-28 10:57:49 +0200

    converted tabs to spaces

--- mergesort.cpp
+++ mergesort.cpp
@@ -34,429 +34,429 @@ WARNING: code contains C++11 */
 namespace boost_filesys = boost::filesystem;
 
 merge_sort_worker::merge_sort_worker(const boost_filesys::path _infile,
-								 	 const boost_filesys::path _outfile,
-								 	 const boost_filesys::path _chunk_path,
-									 const sortmode _sort_mode,
-									 const bool _delete_double_lines,
-									 const unsigned int _lines_per_file,
-									 const std::string _newline
-									 ) {
-	set_infile(_infile);
-	set_outfile(_outfile);
-	set_chunk_path(_chunk_path);
-	set_lines_per_file(_lines_per_file);
-	set_sort_mode(_sort_mode);
-	set_lineend(_newline);
+                                     const boost_filesys::path _outfile,
+                                     const boost_filesys::path _chunk_path,
+                                     const sortmode _sort_mode,
+                                     const bool _delete_double_lines,
+                                     const unsigned int _lines_per_file,
+                                     const std::string _newline
+                                     ) {
+    set_infile(_infile);
+    set_outfile(_outfile);
+    set_chunk_path(_chunk_path);
+    set_lines_per_file(_lines_per_file);
+    set_sort_mode(_sort_mode);
+    set_lineend(_newline);
 
-	lines_before = 0;
-	lines_after = 0;
+    lines_before = 0;
+    lines_after = 0;
 }
 
 merge_sort_worker::~merge_sort_worker() {};
 
 bool merge_sort_worker::ready(void) const {
-	return infile_path.empty() == false &&
-		   outfile_path.empty() == false &&
-		   chunk_path.empty() == false;
+    return infile_path.empty() == false &&
+           outfile_path.empty() == false &&
+           chunk_path.empty() == false;
 }
 
 
 void merge_sort_worker::set_infile(const boost_filesys::path path) {
-	infile_path = path;
+    infile_path = path;
 }
 
 void merge_sort_worker::set_infile(const std::string path) {
-	infile_path = boost_filesys::path(path);
+    infile_path = boost_filesys::path(path);
 }
 
 void merge_sort_worker::set_infile(const char* path) {
-	infile_path = boost_filesys::path(path);
+    infile_path = boost_filesys::path(path);
 }
 
 
 void merge_sort_worker::set_outfile(const boost_filesys::path path) {
-	outfile_path = path;
+    outfile_path = path;
 }
 
 void merge_sort_worker::set_outfile(const std::string path) {
-	outfile_path = boost_filesys::path(path);
+    outfile_path = boost_filesys::path(path);
 }
 
 void merge_sort_worker::set_outfile(const char* path) {
-	outfile_path = boost_filesys::path(path);
+    outfile_path = boost_filesys::path(path);
 }
 
 
 void merge_sort_worker::set_chunk_path(const boost_filesys::path path) {
-	chunk_path = path;
+    chunk_path = path;
 }
 
 void merge_sort_worker::set_chunk_path(const std::string path) {
-	chunk_path = boost_filesys::path(path);
+    chunk_path = boost_filesys::path(path);
 }
 
 void merge_sort_worker::set_chunk_path(const char* path) {
-	chunk_path = boost_filesys::path(path);
+    chunk_path = boost_filesys::path(path);
 }
 
 
 void merge_sort_worker::set_sort_mode(const sortmode sm) {
-	sort_mode = sm;
+    sort_mode = sm;
 }
 
 bool merge_sort_worker::set_lines_per_file(unsigned int n)
 {
-	if (n == 0) {
-		return true;
-	}
-	
-	lines_per_chunk = n;
-	return false;
+    if (n == 0) {
+        return true;
+    }
+    
+    lines_per_chunk = n;
+    return false;
 }
 
 bool merge_sort_worker::set_lineend(std::string _lineend)
 {
-	if (_lineend == "")
-	{
-		newline = "\n";
-		return true;
-	}
+    if (_lineend == "")
+    {
+        newline = "\n";
+        return true;
+    }
 
-	newline = _lineend;
+    newline = _lineend;
 
-	return false;
+    return false;
 }
 
 bool merge_sort_worker::set_lineend(const char* _lineend)
 {
-	if (strcmp(_lineend, ""))
-	{
-		newline = "\n";
-		return true;
-	}
+    if (strcmp(_lineend, ""))
+    {
+        newline = "\n";
+        return true;
+    }
 
-	newline = _lineend;
+    newline = _lineend;
 
-	return false;
+    return false;
 }
 
 void merge_sort_worker::set_delete_double_lines(bool ddl) {
-	delete_double_lines = ddl;
+    delete_double_lines = ddl;
 }
 
 // get
 uint32_t merge_sort_worker::get_lines_before(void) const {
-	return lines_before;
+    return lines_before;
 }
 
 uint32_t merge_sort_worker::get_lines_after(void) const {
-	return lines_after;
+    return lines_after;
 }
 
 uint32_t merge_sort_worker::get_deleted_lines(void) const {
-	return lines_before - lines_after;
+    return lines_before - lines_after;
 }
 
 void merge_sort_worker::tidy_up(std::vector<std::string>& string_vector) const
 {
-	/* remove double strings from vector */
+    /* remove double strings from vector */
 
-	if (delete_double_lines == false) {
-		return;
-	}
-	
-	std::vector<std::string> help_vector;
-	bool found;
-	
-	for (std::string & line : string_vector)
-	{
-		if (line.empty()) { // delete empty lines
-			continue;
-		}
-		
-		found = false;
+    if (delete_double_lines == false) {
+        return;
+    }
+    
+    std::vector<std::string> help_vector;
+    bool found;
+    
+    for (std::string & line : string_vector)
+    {
+        if (line.empty()) { // delete empty lines
+            continue;
+        }
+        
+        found = false;
 
-		for (std::string & j : help_vector) // check if line is more than one time in vector
-		{
-			if (line == j) {
-				found = true;
-				break;
-			}
-		}
+        for (std::string & j : help_vector) // check if line is more than one time in vector
+        {
+            if (line == j) {
+                found = true;
+                break;
+            }
+        }
 
-		if (found == false) {
-			help_vector.push_back(line);
-		}
-	}
+        if (found == false) {
+            help_vector.push_back(line);
+        }
+    }
 
-	if (string_vector.size() != help_vector.size()) { // don't copy if nothing changes
-		string_vector = help_vector;
-	}
+    if (string_vector.size() != help_vector.size()) { // don't copy if nothing changes
+        string_vector = help_vector;
+    }
 }
 
 std::string merge_sort_worker::file_get_line(boost_filesys::path path,
-										uint64_t pos, uint64_t & next_pos) const
+                                        uint64_t pos, uint64_t & next_pos) const
 {
-	std::ifstream infile;
-	std::string line;
+    std::ifstream infile;
+    std::string line;
 
-	infile.open(path.c_str(), std::ios::binary);
+    infile.open(path.c_str(), std::ios::binary);
 
-	if (infile.is_open() == false) {
-		throw std::runtime_error("Could not open file");
-	}
+    if (infile.is_open() == false) {
+        throw std::runtime_error("Could not open file");
+    }
 
-	infile.seekg(pos);         // set read position
-	safeGetline(infile, line);
-	next_pos = infile.tellg(); // get read position
+    infile.seekg(pos);         // set read position
+    safeGetline(infile, line);
+    next_pos = infile.tellg(); // get read position
 
-	return line;
+    return line;
 }
 
 bool merge_sort_worker::compare_lines(std::string & string1, std::string & string2) const
 {
-	switch (sort_mode) {
-	case (ASC) :
-		return (string1.compare(string2) >= 0);
-		// no break required because return exits function
-	case(DESC) :
-		return (string1.compare(string2) <= 0);
-		// no break required because return exits function
-	}
+    switch (sort_mode) {
+    case (ASC) :
+        return (string1.compare(string2) >= 0);
+        // no break required because return exits function
+    case(DESC) :
+        return (string1.compare(string2) <= 0);
+        // no break required because return exits function
+    }
 
-	return false; // just for prevent warnings
+    return false; // just for prevent warnings
 }
 
 void merge_sort_worker::init_variables(void) {
-	chunk_filenames.clear();
+    chunk_filenames.clear();
 
-	lines_before = 0;
-	lines_after = 0;
+    lines_before = 0;
+    lines_after = 0;
 }
 
 // todo: rename funktion
 // todo: error handling
 void merge_sort_worker::file_to_chunks(void)
 {
-	init_variables();
+    init_variables();
 
-	std::vector<std::string> chunk;
-	unsigned int chunk_filename_counter = 0;
+    std::vector<std::string> chunk;
+    unsigned int chunk_filename_counter = 0;
 
-	std::ifstream infile;
-	std::ofstream chunk_file;
-	std::string line, filename;
-	boost_filesys::path chunk_filename;
+    std::ifstream infile;
+    std::ofstream chunk_file;
+    std::string line, filename;
+    boost_filesys::path chunk_filename;
 
-	bool end_of_file;
-	path_seek path_seek_tmp;
+    bool end_of_file;
+    path_seek path_seek_tmp;
 
-	// TODO: create directory
+    // TODO: create directory
 
-	infile.open(infile_path.c_str());
+    infile.open(infile_path.c_str());
 
-	if (infile.is_open() == false) {
-		throw std::runtime_error("Open infile failed");
-	}
+    if (infile.is_open() == false) {
+        throw std::runtime_error("Open infile failed");
+    }
 
-	while (true)
-	{
-		end_of_file = safeGetline(infile, line).eof();
+    while (true)
+    {
+        end_of_file = safeGetline(infile, line).eof();
 
-		try 
-		{
-			if (end_of_file == false) {
-				chunk.push_back(line);
-			}
+        try 
+        {
+            if (end_of_file == false) {
+                chunk.push_back(line);
+            }
 
-			++lines_before; // count lines for statistics
+            ++lines_before; // count lines for statistics
 
-			// create chunk file if chunk size is reached or 
-			// end of infile is reached
-			if (chunk.size() == lines_per_chunk || end_of_file)
-			{
-				tidy_up(chunk);
+            // create chunk file if chunk size is reached or 
+            // end of infile is reached
+            if (chunk.size() == lines_per_chunk || end_of_file)
+            {
+                tidy_up(chunk);
 
-				std::sort(chunk.begin(), chunk.end());
+                std::sort(chunk.begin(), chunk.end());
 
-				// TODO: do in sort algorithm
-				if (sort_mode == DESC) {
-					std::reverse(chunk.begin(), chunk.end());
-				}
+                // TODO: do in sort algorithm
+                if (sort_mode == DESC) {
+                    std::reverse(chunk.begin(), chunk.end());
+                }
 
-				// if in chunk are no double values
-				if (chunk.size() == lines_per_chunk || end_of_file)
-				{
-					// TODO: generate file name in function
+                // if in chunk are no double values
+                if (chunk.size() == lines_per_chunk || end_of_file)
+                {
+                    // TODO: generate file name in function
 
-					std::ostringstream convert;
-					convert << chunk_filename_counter;
-					filename = convert.str();
-					filename += ".cnk"; // TODO: Macro
+                    std::ostringstream convert;
+                    convert << chunk_filename_counter;
+                    filename = convert.str();
+                    filename += ".cnk"; // TODO: Macro
 
-					chunk_filename = chunk_path;
-					chunk_filename.append(filename);
+                    chunk_filename = chunk_path;
+                    chunk_filename.append(filename);
 
-					// chunk read position 0
-					path_seek_tmp = { chunk_filename, 0 };
-					chunk_filenames.push_back(path_seek_tmp);
+                    // chunk read position 0
+                    path_seek_tmp = { chunk_filename, 0 };
+                    chunk_filenames.push_back(path_seek_tmp);
 
-					chunk_file.open(chunk_filename.string(), std::ios_base::binary); // TODO throw exception if open fails
+                    chunk_file.open(chunk_filename.string(), std::ios_base::binary); // TODO throw exception if open fails
 
-					if (chunk_file.is_open() == false) {
-						throw std::runtime_error("Could not open file");
-					}
+                    if (chunk_file.is_open() == false) {
+                        throw std::runtime_error("Could not open file");
+                    }
 
-					for (const std::string &line : chunk)
-					{
-						// TODO: write lines in string before write to file
+                    for (const std::string &line : chunk)
+                    {
+                        // TODO: write lines in string before write to file
 
-						chunk_file << line << std::endl; // replace with newline
+                        chunk_file << line << std::endl; // replace with newline
 
-						if (chunk_file.good() == false) {
-							throw std::runtime_error("File write error");
-						}
-					}
+                        if (chunk_file.good() == false) {
+                            throw std::runtime_error("File write error");
+                        }
+                    }
 
-					chunk_file.close();
-					chunk.clear();
-					++chunk_filename_counter;
-				}
-			}
-		}
-		catch (std::bad_alloc &exc)
-		{
-			// close last file if is open
+                    chunk_file.close();
+                    chunk.clear();
+                    ++chunk_filename_counter;
+                }
+            }
+        }
+        catch (std::bad_alloc &exc)
+        {
+            // close last file if is open
 
-			if (chunk_file.is_open()) {
-				chunk_file.close();
-			}
+            if (chunk_file.is_open()) {
+                chunk_file.close();
+            }
 
-			infile.close();
+            infile.close();
 
-			// delete all chunk files
-			for (path_seek & path_seek_tmp : chunk_filenames) {
-				boost_filesys::remove(path_seek_tmp.path);
-			}
+            // delete all chunk files
+            for (path_seek & path_seek_tmp : chunk_filenames) {
+                boost_filesys::remove(path_seek_tmp.path);
+            }
 
-			throw exc;
-		}
+            throw exc;
+        }
 
-		if (end_of_file) {
-			break;
-		}
-	}
+        if (end_of_file) {
+            break;
+        }
+    }
 
-	infile.close();
+    infile.close();
 }
 
 void merge_sort_worker::start_merge_sort(void)
 {
-	std::ofstream outfile;
-	std::string next_line, line;
-	uint64_t tmp_next_line_pos;
-	uint64_t next_line_pos;
-	path_seek next_line_path_seek;
+    std::ofstream outfile;
+    std::string next_line, line;
+    uint64_t tmp_next_line_pos;
+    uint64_t next_line_pos;
+    path_seek next_line_path_seek;
 
-	/* binary to prevent converting \n to \r\n (or \r\n to \r\r\n) */
-	outfile.open(outfile_path.string(), std::ios_base::binary);
+    /* binary to prevent converting \n to \r\n (or \r\n to \r\r\n) */
+    outfile.open(outfile_path.string(), std::ios_base::binary);
 
-	try {
-		while (! chunk_filenames.empty())
-		{
-			//get next line
-			std::list<path_seek>::iterator filenames_iter = chunk_filenames.begin();
-			std::list<path_seek>::iterator filenames_end_iter = chunk_filenames.end();
-			std::list<path_seek>::iterator next_line_iter;
-		
-			// first line is next_line on start of the loop
-			next_line = file_get_line((*filenames_iter).path,
-				(*filenames_iter).pos, tmp_next_line_pos);
+    try {
+        while (! chunk_filenames.empty())
+        {
+            //get next line
+            std::list<path_seek>::iterator filenames_iter = chunk_filenames.begin();
+            std::list<path_seek>::iterator filenames_end_iter = chunk_filenames.end();
+            std::list<path_seek>::iterator next_line_iter;
+        
+            // first line is next_line on start of the loop
+            next_line = file_get_line((*filenames_iter).path,
+                (*filenames_iter).pos, tmp_next_line_pos);
 
-			next_line_path_seek = (*filenames_iter);
-			next_line_iter = filenames_iter;
-			next_line_pos = tmp_next_line_pos;
+            next_line_path_seek = (*filenames_iter);
+            next_line_iter = filenames_iter;
+            next_line_pos = tmp_next_line_pos;
 
-			if (filenames_iter != filenames_end_iter) {
-				++filenames_iter;
-			} else {
-				// TODO: error description
-				throw std::logic_error("");
-				break;
-			}
+            if (filenames_iter != filenames_end_iter) {
+                ++filenames_iter;
+            } else {
+                // TODO: error description
+                throw std::logic_error("");
+                break;
+            }
 
-			for (; filenames_iter != filenames_end_iter; ++filenames_iter)
-			{
-				line = file_get_line((*filenames_iter).path,
-					(*filenames_iter).pos, tmp_next_line_pos);
+            for (; filenames_iter != filenames_end_iter; ++filenames_iter)
+            {
+                line = file_get_line((*filenames_iter).path,
+                    (*filenames_iter).pos, tmp_next_line_pos);
 
-				if ( compare_lines(next_line, line) )
-				{
-					next_line = line;
-					next_line_path_seek = (*filenames_iter);
-					next_line_iter = filenames_iter;
-					next_line_pos = tmp_next_line_pos;
-				}
-			}
+                if ( compare_lines(next_line, line) )
+                {
+                    next_line = line;
+                    next_line_path_seek = (*filenames_iter);
+                    next_line_iter = filenames_iter;
+                    next_line_pos = tmp_next_line_pos;
+                }
+            }
 
-			outfile << next_line << newline;
-			next_line_iter->pos = next_line_pos;   // set file read position
-			next_line_path_seek.pos = next_line_pos;
+            outfile << next_line << newline;
+            next_line_iter->pos = next_line_pos;   // set file read position
+            next_line_path_seek.pos = next_line_pos;
 
-			++lines_after; // count lines for statistics
+            ++lines_after; // count lines for statistics
 
-			// delete empty chunk files
+            // delete empty chunk files
 
-			if (boost_filesys::file_size(next_line_path_seek.path) <= next_line_path_seek.pos)
-			{
-				boost_filesys::remove(next_line_path_seek.path); // delete file
+            if (boost_filesys::file_size(next_line_path_seek.path) <= next_line_path_seek.pos)
+            {
+                boost_filesys::remove(next_line_path_seek.path); // delete file
 
-				// delete iterator
+                // delete iterator
 
-				filenames_iter = chunk_filenames.begin();
-				filenames_end_iter = chunk_filenames.end();
+                filenames_iter = chunk_filenames.begin();
+                filenames_end_iter = chunk_filenames.end();
 
-				for (; filenames_iter != filenames_end_iter; ++filenames_iter) {
-					if (filenames_iter->path == next_line_path_seek.path) {
-						chunk_filenames.erase(filenames_iter);
-						break;
-					}
-				}
-			}
-		}
-	} catch (std::bad_alloc &exc) {
-		outfile.close();
-		throw exc;
-	} catch (std::runtime_error &exc) {
-		outfile.close();
-		throw exc;
-	}
+                for (; filenames_iter != filenames_end_iter; ++filenames_iter) {
+                    if (filenames_iter->path == next_line_path_seek.path) {
+                        chunk_filenames.erase(filenames_iter);
+                        break;
+                    }
+                }
+            }
+        }
+    } catch (std::bad_alloc &exc) {
+        outfile.close();
+        throw exc;
+    } catch (std::runtime_error &exc) {
+        outfile.close();
+        throw exc;
+    }
 
-	outfile.close();
+    outfile.close();
 
-	// delete all remaining chunk files, in case of an error
-	if (chunk_filenames.empty() == false)
-	{
-		for (const path_seek & path_seek_tmp : chunk_filenames) {
-			boost_filesys::remove(path_seek_tmp.path);
-		}
-		
-		throw std::logic_error("Chunk files left after sorting");
-	}
+    // delete all remaining chunk files, in case of an error
+    if (chunk_filenames.empty() == false)
+    {
+        for (const path_seek & path_seek_tmp : chunk_filenames) {
+            boost_filesys::remove(path_seek_tmp.path);
+        }
+        
+        throw std::logic_error("Chunk files left after sorting");
+    }
 }
 
 void merge_sort_worker::start_worker(void)
 {
-	if (ready()) {
-		file_to_chunks();
-		start_merge_sort();
-	} else {
-		throw std::runtime_error("Path's not valid");
-	}
+    if (ready()) {
+        file_to_chunks();
+        start_merge_sort();
+    } else {
+        throw std::runtime_error("Path's not valid");
+    }
 }
 
 void merge_sort_worker::operator() () {
-	this->start_worker();
+    this->start_worker();
 }
 
 #endif MERGESORT_CPP
--- mergesort.h
+++ mergesort.h
@@ -34,78 +34,78 @@ namespace boost_filesys = boost::filesystem;
 typedef bool sortmode;
 
 struct path_seek {
-	boost_filesys::path path;
-	uint64_t pos; // file read position
+    boost_filesys::path path;
+    uint64_t pos; // file read position
 };
 
 class merge_sort_worker {
 public:
-	// Constructors:
-	merge_sort_worker(const boost_filesys::path _infile = "",
-					  const boost_filesys::path _outfile = "",
-					  const boost_filesys::path _chunk_path = DEFAUlT_CHUNK_PATH,
-					  const sortmode _sort_mode = DEFAULT_SORT_MODE,
-					  const bool _delete_double_lines = DEFAULT_DELETE_DOUBLE_LINES,
-					  const unsigned int _lines_per_file = DEFAULT_LINES_PER_FILE,
-					  const std::string _newline = DEFAULT_LINE_END
-					  );
+    // Constructors:
+    merge_sort_worker(const boost_filesys::path _infile = "",
+                      const boost_filesys::path _outfile = "",
+                      const boost_filesys::path _chunk_path = DEFAUlT_CHUNK_PATH,
+                      const sortmode _sort_mode = DEFAULT_SORT_MODE,
+                      const bool _delete_double_lines = DEFAULT_DELETE_DOUBLE_LINES,
+                      const unsigned int _lines_per_file = DEFAULT_LINES_PER_FILE,
+                      const std::string _newline = DEFAULT_LINE_END
+                      );
 
-	virtual ~merge_sort_worker();
+    virtual ~merge_sort_worker();
 
-	bool ready(void) const;
+    bool ready(void) const;
 
-	void start_worker(void);
-	void operator() ();
+    void start_worker(void);
+    void operator() ();
 
-	// set-methods
-	void set_infile(boost_filesys::path);
-	void set_infile(std::string);
-	void set_infile(const char*);
+    // set-methods
+    void set_infile(boost_filesys::path);
+    void set_infile(std::string);
+    void set_infile(const char*);
 
-	void set_outfile(boost_filesys::path);
-	void set_outfile(std::string);
-	void set_outfile(const char*);
+    void set_outfile(boost_filesys::path);
+    void set_outfile(std::string);
+    void set_outfile(const char*);
 
-	void set_chunk_path(boost_filesys::path);
-	void set_chunk_path(std::string);
-	void set_chunk_path(const char*);
+    void set_chunk_path(boost_filesys::path);
+    void set_chunk_path(std::string);
+    void set_chunk_path(const char*);
 
-	void set_sort_mode(const sortmode);
+    void set_sort_mode(const sortmode);
 
-	bool set_lines_per_file(unsigned int);
+    bool set_lines_per_file(unsigned int);
 
-	bool set_lineend(std::string);
-	bool set_lineend(const char*);
+    bool set_lineend(std::string);
+    bool set_lineend(const char*);
 
-	void set_delete_double_lines(bool);
+    void set_delete_double_lines(bool);
 
-	// get-methods
-	uint32_t get_lines_before(void) const;
-	uint32_t get_lines_after(void) const;
-	uint32_t get_deleted_lines(void) const;
+    // get-methods
+    uint32_t get_lines_before(void) const;
+    uint32_t get_lines_after(void) const;
+    uint32_t get_deleted_lines(void) const;
 private:
-	void tidy_up(std::vector<std::string>&) const;
-	inline std::string file_get_line(const boost_filesys::path path,
-								uint64_t pos, uint64_t & next_pos) const;
-	bool compare_lines(std::string &, std::string &) const;
+    void tidy_up(std::vector<std::string>&) const;
+    inline std::string file_get_line(const boost_filesys::path path,
+                                uint64_t pos, uint64_t & next_pos) const;
+    bool compare_lines(std::string &, std::string &) const;
 
-	void init_variables(void);
-	void file_to_chunks(void);
-	void start_merge_sort(void);
+    void init_variables(void);
+    void file_to_chunks(void);
+    void start_merge_sort(void);
 
-	// parameter
-	boost_filesys::path infile_path;
-	boost_filesys::path outfile_path;
-	boost_filesys::path chunk_path;
-	unsigned int lines_per_chunk;
-	bool delete_double_lines;
-	std::string newline;
-	sortmode sort_mode;
+    // parameter
+    boost_filesys::path infile_path;
+    boost_filesys::path outfile_path;
+    boost_filesys::path chunk_path;
+    unsigned int lines_per_chunk;
+    bool delete_double_lines;
+    std::string newline;
+    sortmode sort_mode;
 
-	// for programm
-	std::list<path_seek> chunk_filenames;
-	uint32_t lines_before;
-	uint32_t lines_after;
+    // for programm
+    std::list<path_seek> chunk_filenames;
+    uint32_t lines_before;
+    uint32_t lines_after;
 };
 
 #endif MERGESORT_H
